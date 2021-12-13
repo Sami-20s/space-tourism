ee921edafdaf190576a635188075f1f9fd75f2f5
Sami-20s <samikfouri22@gmail.com>
2021-12-14 00:01:02 +0200

    🧭 🎗 Nav Style Added

---
+++ styles/devices.ts
@@ -0,0 +1,11 @@
+const devices = {
+    meduim: `40em`,
+    large: `64em`,
+    xlarge: `87.5em`,
+};
+
+export const media = {
+    meduim: `(min-width:${devices.meduim})`,
+    large: `(min-width:${devices.large})`,
+    xlarge: `(min-width:${devices.xlarge})`,
+};
