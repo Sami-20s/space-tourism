a30d1f64cbadab227d26644cb6fecf4f38348dd4
Sami-20s <samikfouri22@gmail.com>
2021-12-15 20:55:14 +0200

    🧭 🔧 Fix Some Bugs in Nav

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -1,35 +1,38 @@
 import React, { useState } from "react";
 import { useGlobal } from "../../context/context";
-import { Burger, Logo, NavContainer, NavStyle } from "../../styles/Nav.style";
+import {
+    Burger,
+    Logo,
+    NavContainer,
+    NavStyle,
+} from "../../styles/NavStyle/Nav.style";
 import NavLinks from "./NavLinks";
 
 const Nav = () => {
     const { navLinks } = useGlobal();
     const [isBurgerClick, setIsBurgerClick] = useState(false);
     return (
-        <nav style={{ overflowX: "hidden" }}>
-            <NavStyle>
-                <NavContainer>
-                    <Logo href="/">
-                        <img src="/assets/shared/logo.svg" alt="logo" />
-                    </Logo>
-                    <NavLinks
-                        list={navLinks}
-                        isBurgerClick={isBurgerClick}
-                        setIsBurgerClick={setIsBurgerClick}
+        <NavStyle>
+            <NavContainer>
+                <Logo href="/">
+                    <img src="/assets/shared/logo.svg" alt="logo" />
+                </Logo>
+                <NavLinks
+                    list={navLinks}
+                    isBurgerClick={isBurgerClick}
+                    setIsBurgerClick={setIsBurgerClick}
+                />
+                <Burger>
+                    <img
+                        src="/assets/shared/icon-hamburger.svg"
+                        alt=""
+                        onClick={() => {
+                            setIsBurgerClick(true);
+                        }}
                     />
-                    <Burger>
-                        <img
-                            src="/assets/shared/icon-hamburger.svg"
-                            alt=""
-                            onClick={() => {
-                                setIsBurgerClick(true);
-                            }}
-                        />
-                    </Burger>
-                </NavContainer>
-            </NavStyle>
-        </nav>
+                </Burger>
+            </NavContainer>
+        </NavStyle>
     );
 };
 
--- components/Nav/NavLinks.tsx
+++ components/Nav/NavLinks.tsx
@@ -1,5 +1,5 @@
 import React, { FC, useState } from "react";
-import { NavLinksStyle } from "../../styles/Nav.style";
+import { NavLinksStyle } from "../../styles/NavStyle/Nav.style";
 import { NavLinksProps } from "../../types/Nav/NavTypes";
 import NavSingleLink from "./NavSingleLink";
 
