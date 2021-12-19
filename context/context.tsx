79ea1aaf07cac67ac63dd991e3aca45c988a1c8e
Sami-20s <samikfouri22@gmail.com>
2021-12-19 19:26:38 +0200

    🛣 🔌 Destination Context Added

--- context/context.tsx
+++ context/context.tsx
@@ -1,14 +1,22 @@
 import React, { useContext, useState } from "react";
 import { ContextTypes } from "../types/context";
 import { navLinks } from "./navList/navList";
+import { planetsList } from "./planetsList/planetsList";
 
-const ContextApp: ContextTypes = React.createContext({ navLinks });
+const ContextApp: ContextTypes = React.createContext({
+    navLinks,
+    planetsList,
+});
 const ContextProvider = ({ children }) => {
     const [isLinkClick, setIsLinkClick] = useState("");
+    const [click, setClick] = useState("00");
     return (
         <ContextApp.Provider
             value={{
                 navLinks,
+                planetsList,
+                click,
+                setClick,
             }}
         >
             {children}
---
+++ context/planetsList/planetsList.ts
@@ -0,0 +1,40 @@
+import { DestinationListType } from "../../types/Destination/DestinationTypes";
+
+export const planetsList: DestinationListType = [
+    {
+        id: 1,
+        title: "MOON",
+        url: "/assets/destination/image-moon.webp",
+        description:
+            "See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.",
+        dist: "384,400 km",
+        time: "3 days",
+    },
+    {
+        id: 2,
+        title: "MARS",
+        url: "/assets/destination/image-mars.webp",
+        description:
+            "Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!",
+        dist: "225 MIL. km",
+        time: "9 months",
+    },
+    {
+        id: 3,
+        title: "EUROPA",
+        url: "/assets/destination/image-europa.webp",
+        description:
+            "The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.",
+        dist: "628 MIL. km",
+        time: "3 years",
+    },
+    {
+        id: 4,
+        title: "TITAN",
+        url: "/assets/destination/image-titan.webp",
+        description:
+            "The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.",
+        dist: "1.6 BIL. km",
+        time: "7 years",
+    },
+];
